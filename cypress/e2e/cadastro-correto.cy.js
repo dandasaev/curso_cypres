4499e436bf0e2a3043f6640fefd49c9ee5545f1c
Daniel Dasaev <dandasaev@gmail.com>
2026-06-18 18:49:21 -0300

    corecoes em testes

--- cypress/e2e/cadastro-correto.cy.js
+++ cypress/e2e/cadastro-correto.cy.js
@@ -1,15 +1,11 @@
-describe('pagina de cadastro', () => {
-  it('Preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
-    cy.visit('https://adopet-frontend-cypress.vercel.app/');
-    cy.contains('a', 'Cadastrar').click();
-    cy.get('[data-test="input-name"]').type('Joao da Silva');
-    cy.get('[data-test="input-email"]').type('Joao.da.Silva@email.com');
-    cy.get('[data-test="input-password"]').type('Senha123456');
-    cy.get('[data-test="input-confirm-password"]').type('Senha123456');
-    cy.get('[data-test="submit-button"]').click();
+describe('Página de cadastro', () => {
+  it('Deve preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
+       cy.visit('https://adopet-frontend-cypress.vercel.app/');
+        cy.get('[data-test="register-button"]').click();
+        cy.get('[data-test="input-name"]').type('Marjorie Estiano');
+        cy.get('[data-test="input-email"]').type('marjorie2@email.com');
+       cy.get('[data-test="input-password"]').type('Senha123');
+        cy.get('[data-test="input-confirm-password"]').type('Senha123');
+        cy.get('[data-test="submit-button"]').click();
   })
-
-    
-
-
 })
